697ff465e12d431c4eb6a76c4ddf8691cd016b0b
A-delta <louis.tvnt@gmail.com>
2021-03-14 14:50:08 +0100

    Cleaned class

--- SharpCalculator/Calculator.cs
+++ SharpCalculator/Calculator.cs
@@ -14,22 +14,8 @@ namespace VarCalculator
         public void ProcessExpression(String expression)
         {
             Console.WriteLine("\nProcessing : " + expression);
+          
 
-            // if fcn in expression : do fcn
-
-            if (expression.Contains('*') || expression.Contains('/'))
-            {
-
-            }
-
-            
-
-        }
-
-
-        private int _Sum(int x, int y)
-        {
-            return x + y;
         }
 
         ~Calculator()
