3fc5a1d6fc83df3b8e399f91945ebe1c15f510ad
A-delta <louis.tvnt@gmail.com>
2021-03-14 14:30:23 +0100

    Add project files.

---
+++ SharpCalculator/Calculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VarCalculator
+{
+    class Calculator
+    {
+        public Calculator()
+        {
+            Console.WriteLine("Calculator initialised");
+        }
+
+        public void ProcessExpression(String expression)
+        {
+            Console.WriteLine("\nProcessing : " + expression);
+
+            if (expression.Contains("=="))
+            {
+                _isEqual(expression);
+            }
+
+            else if (expression.Contains("+"))
+            {
+
+            }
+
+
+        }
+
+        private void _isEqual(String expression)
+        {
+            String[] members = expression.Split("==");
+
+            List<int> items = new List<int>();
+            foreach (String member in members)
+            {
+                items.Add(int.Parse(member));
+            }
+
+            foreach (int i in items)
+            {
+                foreach (int o in items)
+                {
+                    if (i != o)
+                    {
+                        Console.WriteLine("FALSE");
+                        return;
+                    }
+                }
+                Console.WriteLine("TRUE");
+                return;
+
+            }
+        }
+
+        ~Calculator()
+        {
+            Console.WriteLine("Calculator deleted");
+        }
+
+    }
+}
---
+++ SharpCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VarCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.ProcessExpression("1==1");
+            calc.ProcessExpression("1==2");
+            calc.ProcessExpression("4==5==4");
+            calc.ProcessExpression("2==2==2");
+        }
+    }
+}
