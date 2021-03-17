0e89a6f533f00c162fef1c319cc298f50635be46
A-delta <louis.tvnt@gmail.com>
2021-03-17 10:10:37 +0100

    Added support for negative numbers

--- SharpCalculator/Calculator.cs
+++ SharpCalculator/Calculator.cs
@@ -23,34 +23,72 @@ namespace SharpCalculator
         private List<String> _CleanInfix(String expression)
         {
             List<String> cleanedInfixExpression = new List<string>();
-            bool lastWasNumeric = false;
+            int last = 0;  // 0 : space ; 1 : num ; 2 : operator ; 3 : () ; 4 ; fcn
 
-            char[] charachters = expression.ToCharArray();
-            foreach (char token in charachters)
+            char[] characters = expression.ToCharArray();
+            foreach (char token in characters)
             {
+                if (last == 2 && "-+*/".Contains(token))
+                {
+                    Exception error = new Exception("Error in input expression."+token);
+                    Console.WriteLine(error);
+                    
+                }
+
                 if (token == ' ')
                 {
-                    lastWasNumeric = false;
                     continue;
                 }
 
                 else if ("0123456789".Contains(token))
                 {
-                    if (lastWasNumeric)
+                    if (last == 1)
                         cleanedInfixExpression[cleanedInfixExpression.Count-1] += token.ToString();
                     else
                     {
                         cleanedInfixExpression.Add(token.ToString());
-                        lastWasNumeric = true;
+                        last = 1;
                     }
                 }
-                else
+                else if ("()".Contains(token))
                 {
-                    lastWasNumeric = false;
+
+
                     cleanedInfixExpression.Add(token.ToString());
+
+                    last = 3;
+                }
+
+                else if ("-+*/".Contains(token))
+                {
+                    if (token == '-' && last != 1)
+                    {
+                        cleanedInfixExpression.Add("0");
+                    }
+
+                    cleanedInfixExpression.Add(token.ToString());
+                    last = 2;
+                }
+
+                else if (97 <= (int)token && (int)token <= 122)
+                {
+                    /*if (last==4)
+                    {
+                        cleanedInfixExpression[cleanedInfixExpression.Count-1] += token.ToString();
+                    }
+                    else
+                    { 
+                        cleanedInfixExpression.Add(token.ToString());
+                    }
+
+                    last = 4;*/
                 }
             }
-
+            foreach (string a in cleanedInfixExpression)
+            {
+                Console.Write(a);
+            }
+            Console.WriteLine();
             return cleanedInfixExpression;
         }
 
