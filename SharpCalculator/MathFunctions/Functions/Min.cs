e24dbf545410e2037cd5bce2196538719eb7ff53
A-delta <louis.tvnt@gmail.com>
2021-03-28 10:42:52 +0200

    Added Min() function

---
+++ SharpCalculator/MathFunctions/Functions/Min.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpCalculator.MathFunctions
+{
+    public class Min : IFunction
+    {
+        private int _argumentsCount = 2;
+        public int ArgumentsCount
+        {
+            get => _argumentsCount;
+
+        }
+
+        private String _infixOperator = "None";
+        public String InfixOperator
+        {
+            get => _infixOperator;
+
+        }
+
+        private int _infixOperatorPriority = 0;
+        public int InfixOperatorPriority
+        {
+            get => _infixOperatorPriority;
+
+        }
+
+
+
+        private List<String> _aliases = new List<string>();
+
+        public List<String> getAliases()
+        {
+            _aliases.Add("min");
+            return _aliases;
+        }
+
+        public double ExecuteFunction(List<Double> args)
+        {
+            return Math.Min(args[0], args[1]);
+        }
+
+    }
+
+}
