c907bcc71466bca1a43d68047bc6167dab40003e
A-delta <louis.tvnt@gmail.com>
2021-04-11 12:04:52 +0200

    added < > =< => functions to calc

---
+++ SharpCalculatorLib/MathFunctions/Prefix_Functions/PGCD.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCalculatorLib.MathFunctions
+{
+    class PGCD : IFunction
+    {
+        private String _docstring = "Returns PGCD of two values";
+        public String Docstring
+        {
+            get => _docstring;
+
+        }
+
+
+        private int _argumentsCount = 2;
+        public int ArgumentsCount
+        {
+            get => _argumentsCount;
+
+        }
+
+        private String _infixOperator = "None";
+        public String InfixOperator
+        {
+            get => _infixOperator;
+
+        }
+
+        private int _infixOperatorPriority = 0;
+        public int InfixOperatorPriority
+        {
+            get => _infixOperatorPriority;
+
+        }
+
+
+
+        private List<String> _aliases = new List<string>();
+
+        public List<String> getAliases()
+        {
+            _aliases.Add("pgcd");
+            return _aliases;
+        }
+
+        public string ExecuteFunction(State state, List<string> args)
+        {
+            int a;
+            int b;
+            try
+            {
+                a = Math.Abs(int.Parse(args[1]));
+                b = Math.Abs(int.Parse(args[0]));
+            }
+            catch(Exception)
+            {
+                throw new ArgumentException("Only integers are allowed in arithmetic");
+            }
+
+            if (b==0) { return a.ToString(); }
+            if (b == 1) { return "1"; }
+            if (a % b == 0) { return b.ToString(); }
+
+            int r = a % b;
+
+            while (a % b != 0)
+            {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+            return r.ToString();
+        }
+    }
+}
---
+++ SharpCalculatorLib/MathFunctions/Tests/IsHigher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpCalculatorLib.MathFunctions
+{
+    class IsHigher : IFunction
+    {
+        private String _docstring = "Returns true if arguments are >";
+        public String Docstring
+        {
+            get => _docstring;
+
+        }
+
+        private int _argumentsCount = 2;
+        public int ArgumentsCount
+        {
+            get => _argumentsCount;
+
+        }
+
+
+
+        private String _infixOperator = ">";
+        public String InfixOperator
+        {
+            get => _infixOperator;
+
+        }
+
+        private int _infixOperatorPriority = 2;
+        public int InfixOperatorPriority
+        {
+            get => _infixOperatorPriority;
+
+        }
+
+
+        private List<String> _aliases = new List<string>();
+
+        public List<String> getAliases()
+        {
+            _aliases.Add("isHigher");
+            _aliases.Add(">");
+            return _aliases;
+        }
+
+        public string ExecuteFunction(State state, List<string> args)
+        {
+
+            double arg1 = Double.Parse(args[1]);
+            double arg2 = Double.Parse(args[0]);
+
+            return (arg1 > arg2).ToString();
+        }
+    }
+}
