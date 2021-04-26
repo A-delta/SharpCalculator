50c2544884d40867b605a55c48d46b48bfbf43a3
A-delta <louis.tvnt@gmail.com>
2021-04-26 19:33:39 +0200

    Added logarithm functions

---
+++ SharpCalculatorLib/MathFunctions/Prefix_Functions/DecimalLogarithm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpCalculatorLib.MathFunctions
+{
+    internal class DecimalLogarithm : IFunction
+    {
+        private String _docstring = "Returns log of a value with base 10";
+
+        public String Docstring
+        {
+            get => _docstring;
+        }
+
+        private int _argumentsCount = 1;
+
+        public int ArgumentsCount
+        {
+            get => _argumentsCount;
+        }
+
+        private String _postfixOperator = "None";
+
+        public String PostfixOperator
+        {
+            get => _postfixOperator;
+        }
+
+        private String _infixOperator = "None";
+
+        public String InfixOperator
+        {
+            get => _infixOperator;
+        }
+
+        private int _OperatorPriority = 0;
+
+        public int OperatorPriority
+        {
+            get => _OperatorPriority;
+        }
+
+        private List<String> _aliases = new List<string>();
+
+        public List<String> getAliases()
+        {
+            _aliases.Add("log");
+            return _aliases;
+        }
+
+        public string ExecuteFunction(State state, List<string> args)
+        {
+            double arg1 = VarNumberConverter.GetNumber(state, args[0]);
+
+            return Math.Log10(arg1).ToString();
+        }
+    }
+}
